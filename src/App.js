91c426ae431e8d27e27e424850a222633de160ef
HotPepper970319 <101436590+HotPepper970319@users.noreply.github.com>
2025-12-27 01:41:34 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,12 +21,12 @@ import {
 
 // --- 重要：請在此填入你的真實 Firebase 配置 ---
 const firebaseConfig = {
-  apiKey: "你的_API_KEY",
-  authDomain: "你的_PROJECT_ID.firebaseapp.com",
-  projectId: "你的_PROJECT_ID",
-  storageBucket: "你的_PROJECT_ID.appspot.com",
-  messagingSenderId: "你的_SENDER_ID",
-  appId: "你的_APP_ID"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const appId = "my-vocab-app";
